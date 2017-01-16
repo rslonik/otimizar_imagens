64b23ff32ce203c53d245f795445d3c6b3d442c4
Rafael <rslonik@gmail.com>
2017-01-16 11:09:03 -0500

    alterado para pegar a /uploads/ de todos os sites da vps e rodar apenas no mês corrente

--- otimizar_imagens.sh
+++ otimizar_imagens.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-if [ -z $1 ] 
-then
-    echo "USO: ./otimizar_imagens.sh caminho";
-    exit 1;
-fi
-
-for file in $(find $1 -type f -name "*.jpg")
-do
-        jpegoptim --strip-all -m75 -o $file
-done
-
-
-for file in $(find $1 -type f -name "*.png")
-do
-        zopflipng -y $file $file
-done
+find /var/www/*/htdocs/wp-content/uploads/$(date +%Y/%m) -type f -name '*.jpg' -exec jpegoptim --strip-all -m75 -o {} \;
+find /var/www/*/htdocs/wp-content/uploads/$(date +%Y/%m) -type f -name '*.png' -exec zopflipng -y {} {} \;
